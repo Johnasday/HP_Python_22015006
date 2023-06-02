ac5a9c0255189cbe33957124278d1b5d36398094
Johnasday <133768846+Johnasday@users.noreply.github.com>
2023-06-02 06:21:00 +0100

    Create 3-incremental_shape.py
    Third Assignment

---
+++ 3-incremental_shape.py
@@ -0,0 +1,14 @@
+# for koop that transverse number from 1 to 100
+for i in range (1,101):
+    # check if the number is divisible by both 3 and 5
+    if(i%3==0 and i%5==0):
+       print("couple",end=",")
+    # check if the number is divisible by 3
+    elif(i%3==0):
+       print("boys",end=",")
+    # check if the number is divisible by 5
+    elif(i%5==0):
+       print("girls",end=",")
+    # check if the number is divisible by either of them print the i
+    else:
+       print(i,end=",")
